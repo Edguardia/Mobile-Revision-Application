9958889339768938eb2c06320302766137d51be3
Edguardia <egwpmpp9@gmail.com>
2022-07-27 18:11:17 +0100

    Login Screen Start

--- main.py
+++ main.py
@@ -2,12 +2,35 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text="User Name"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text="Password"))
+        self.password = TextInput(multiline=False)
+        self.add_widget(self.password)
+
+
+
+
+
 
 
 class MyApp(App):
 
     def build(self):
-        return Label(text="Hello World!")
+        return LoginScreen()
 
 if __name__ == '__main__':
     MyApp().run()
+
+
